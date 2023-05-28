9b8d32b90183d5c55794d17f955aed45729e5b7f
Vinicius Lago Gehrke <vinictus@gmail.com>
2023-05-29 00:05:03 +0200

    Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import { ColorModeContext, useMode } from 'theme'
 import { CssBaseline, ThemeProvider } from '@mui/material'
-import Header from './layout/header'
+import { Routes, Route } from 'react-router-dom'
+import Header from './layout/Header'
+import Sidebar from './layout/Sidebar'
+import Dashboard from './pages/Dashboard'
+// import Team from './pages/Team'
+// import Contacts from './pages/Contacts'
+// import Invoices from './pages/Invoices'
+// import Bar from './pages/Bar'
+// import Form from './pages/Form'
+// import Line from './pages/Line'
+// import Pie from './pages/Pie'
+// import FAQ from './pages/FAQ'
+// import Geography from './pages/Geography'
+// import Calendar from './pages/Calendar'
 
 function App() {
   const { theme, colorMode } = useMode()
@@ -9,9 +22,23 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <div className="flex h-full w-full flex-col items-center justify-center">
+        <div className="flex h-full w-full">
+          <Sidebar />
           <main className="w-full">
             <Header />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
