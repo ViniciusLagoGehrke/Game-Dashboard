6c7a8a2d941fbd172e10e2200c98d1928800d07a
Vinicius Lago Gehrke <vinictus@gmail.com>
2023-05-30 17:28:13 +0200

    added Pie Chart

--- src/pages/Pie/Pie.tsx
+++ src/pages/Pie/Pie.tsx
@@ -1,3 +1,16 @@
-export default function Pie() {
-  return <div>Pie</div>
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
+import PieChart from '../../components/PieChart'
+
+const Pie = () => {
+  return (
+    <Box m="20px">
+      <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+      <Box height="75vh">
+        <PieChart />
+      </Box>
+    </Box>
+  )
 }
+
+export default Pie
